9cdd3b9619c74c48b906edb7c7e3398890f0c6f5
vicky fadilla <vicky.yuliawan@ist.id>
2024-11-22 12:56:58 +0700

    disable eslint

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false, // Disable React Strict Mode
+  eslint: {
+    // Disables eslint during build time
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
